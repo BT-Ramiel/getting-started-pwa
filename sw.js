1d55869b86e9b43fe4e9d7203ca0219da5d421ab
Xenofixion <davila.bojorquez@gmail.com>
2021-11-13 16:34:17 -0600

    :art: added prettier configuration

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener("install", (event) => {
-  console.log("Service worker installed");
-});
+self.addEventListener('install', (event) => {
+  console.log('Service worker installed')
+})
